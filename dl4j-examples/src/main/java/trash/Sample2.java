746bdc7cceb8cf3b263afb602773fc69de014419
acastano <acastano@GYECDOC6PC16.ups.edu.ec>
2016-09-15 15:42:49 -0500

    probando 2

--- dl4j-examples/src/main/java/trash/Sample2.java
+++ dl4j-examples/src/main/java/trash/Sample2.java
@@ -22,9 +22,26 @@ import org.datavec.image.transform.ScaleImageTransform;
 import org.datavec.image.transform.ShowImageTransform;
 import org.datavec.image.transform.WarpImageTransform;
 import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+import org.deeplearning4j.eval.Evaluation;
 import static org.deeplearning4j.examples.dataExamples.ImagePipelineExample.randNumGen;
+import static org.deeplearning4j.examples.feedforward.regression.RegressionMathFunctions.iterations;
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.Updater;
+import org.deeplearning4j.nn.conf.layers.ConvolutionLayer;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.conf.layers.SubsamplingLayer;
+import org.deeplearning4j.nn.conf.layers.setup.ConvolutionLayerSetup;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
 
 /**
  *
@@ -33,7 +50,11 @@ import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 public class Sample2 {
 
     public static void main(String[] args) {
+        
+        // 
         try {
+            int nChannels = 3;
+            
             File parentDir = new File("C:\\Users\\acastano\\Downloads\\lfw\\lfw\\lfw");
             String[] allowedExtensions = new String[]{"jpg"};
             FileSplit filesInDir = new FileSplit(parentDir, allowedExtensions, randNumGen);
@@ -46,11 +67,11 @@ public class Sample2 {
             InputSplit testData = filesInDirSplit[1];
             
             
-            ImageRecordReader recordReader = new ImageRecordReader(28, 28, 3, labelMaker);
-//        int outputNum = 0;
+            ImageRecordReader recordReader = new ImageRecordReader(28, 28, nChannels, labelMaker);
+        int outputNum = 5749;
             
             recordReader.initialize(trainData);
-            DataSetIterator dataIter = new RecordReaderDataSetIterator(recordReader, 10, 784, 5749);
+            DataSetIterator dataIter = new RecordReaderDataSetIterator(recordReader, 10, 784, outputNum);
             
             int contador = 0;
 //            while (dataIter.hasNext()) {
@@ -60,6 +81,101 @@ public class Sample2 {
 //            }
             
             System.out.println("Num Clases: "+dataIter.getLabels().size());
+            int seed = 0;
+            
+            
+            
+            
+            
+        
+            
+            
+            MultiLayerConfiguration.Builder builder = new NeuralNetConfiguration.Builder()
+                .seed(seed)
+                .iterations(iterations)
+                .regularization(true).l2(0.0005)
+                .learningRate(0.01)//.biasLearningRate(0.02)
+                //.learningRateDecayPolicy(LearningRatePolicy.Inverse).lrPolicyDecayRate(0.001).lrPolicyPower(0.75)
+                .weightInit(WeightInit.XAVIER)
+                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+                .updater(Updater.NESTEROVS).momentum(0.9)
+                .list()
+                .layer(0, new ConvolutionLayer.Builder(5, 5)
+                        //nIn and nOut specify depth. nIn here is the nChannels and nOut is the number of filters to be applied
+                        .nIn(nChannels)
+                        .stride(1, 1)
+                        .nOut(20)
+                        .activation("identity")
+                        .build())
+                .layer(1, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
+                        .kernelSize(2,2)
+                        .stride(2,2)
+                        .build())
+                .layer(2, new ConvolutionLayer.Builder(5, 5)
+                        //Note that nIn need not be specified in later layers
+                        .stride(1, 1)
+                        .nOut(50)
+                        .activation("identity")
+                        .build())
+                .layer(3, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
+                        .kernelSize(2,2)
+                        .stride(2,2)
+                        .build())
+                .layer(4, new DenseLayer.Builder().activation("relu")
+                        .nOut(500).build())
+                .layer(5, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                        .nOut(outputNum)
+                        .activation("softmax")
+                        .build())
+                .backprop(true).pretrain(false);
+        // The builder needs the dimensions of the image along with the number of channels. these are 28x28 images in one channel
+        new ConvolutionLayerSetup(builder,28,28,1);
+
+        MultiLayerConfiguration conf = builder.build();
+        MultiLayerNetwork model = new MultiLayerNetwork(conf);
+        model.init();
+
+
+//        log.info("Train model....");
+        model.setListeners(new ScoreIterationListener(1));
+            int nEpochs = 2;
+            
+            Nd4j.ENFORCE_NUMERICAL_STABILITY = true;
+        for( int i=0; i<nEpochs; i++ ) {
+            model.fit(dataIter);
+//            log.info("*** Completed epoch {} ***", i);
+//
+//            log.info("Evaluate model....");
+//            Evaluation eval = new Evaluation(outputNum);
+//            while(mnistTest.hasNext()){
+//                DataSet ds = mnistTest.next();
+//                INDArray output = model.output(ds.getFeatureMatrix(), false);
+//                eval.eval(ds.getLabels(), output);
+//            }
+//            log.info(eval.stats());
+//            mnistTest.reset();
+        }
+//        log.info("****************Example finished********************");
+
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
         } catch (IOException ex) {
             Logger.getLogger(Sample2.class.getName()).log(Level.SEVERE, null, ex);
         }
