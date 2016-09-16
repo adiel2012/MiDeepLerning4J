ca28edf2d1bbd5b6c683c7ae47e827b6ffb57860
acastano <acastano@GYECDOC6PC16.ups.edu.ec>
2016-09-15 19:12:00 -0500

    Revert "probando 5"
    This reverts commit 8393d9d12676a82e7484c627a612e069acd45a20.

--- dl4j-examples/src/main/java/trash/CNNLFWExample.java
+++ dl4j-examples/src/main/java/trash/CNNLFWExample.java
@@ -4,7 +4,14 @@
  * and open the template in the editor.
  */
 package trash;
-import org.canova.image.loader.LFWLoader;
+
+/**
+ *
+ * @author acastano
+ */
+import java.io.File;
+import java.io.IOException;
+//import org.canova.image.loader.LFWLoader;
 import org.deeplearning4j.datasets.iterator.DataSetIterator;
 import org.deeplearning4j.datasets.iterator.impl.LFWDataSetIterator;
 import org.deeplearning4j.eval.Evaluation;
@@ -33,6 +40,15 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import java.util.logging.Level;
+import org.datavec.api.io.filters.BalancedPathFilter;
+import org.datavec.api.io.labels.ParentPathLabelGenerator;
+import org.datavec.api.split.FileSplit;
+import org.datavec.api.split.InputSplit;
+import org.datavec.image.recordreader.ImageRecordReader;
+import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+import static org.deeplearning4j.examples.dataExamples.ImagePipelineExample.randNumGen;
+import static org.deeplearning4j.examples.feedforward.regression.RegressionMathFunctions.iterations;
 
 /**
  * Reference: architecture partially based on DeepFace: http://mmlab.ie.cuhk.edu.hk/pdf/YiSun_CVPR14.pdf
@@ -48,112 +64,153 @@ public class CNNLFWExample {
 
     public static void main(String[] args) {
 
-
-        final int numRows = 40;
-        final int numColumns = 40;
-        int nChannels = 3;
-        int outputNum = LFWLoader.SUB_NUM_LABELS;
-        int numSamples = LFWLoader.SUB_NUM_IMAGES-4;
-        int batchSize = numSamples/10;
-        int iterations = 5;
-        int splitTrainNum = (int) (batchSize*.8);
-        int seed = 123;
-        int listenerFreq = iterations/5;
-        boolean useSubset = true;
-        DataSet lfwNext;
-        SplitTestAndTrain trainTest;
-        DataSet trainInput;
-        List<INDArray> testInput = new ArrayList<>();
-        List<INDArray> testLabels = new ArrayList<>();
-        //int numlabels = ;
-
-
-//        log.info("Load data....");
-        LFWDataSetIterator lfw = new LFWDataSetIterator(batchSize, numSamples, outputNum, useSubset, splitTrainNum);
-        //(batchSize, numSamples, new int[] {numRows, numColumns, nChannels}, outputNum, useSubset, new Random(seed));
-
-//        log.info("Build model....");
-        MultiLayerConfiguration.Builder builder = new NeuralNetConfiguration.Builder()
+        
+        
+        try {
+            int nChannels = 3;
+            
+            File parentDir = new File("C:\\Users\\acastano\\Downloads\\lfw\\lfw\\lfw");
+            String[] allowedExtensions = new String[]{"jpg"};
+            FileSplit filesInDir = new FileSplit(parentDir, allowedExtensions, randNumGen);
+            ParentPathLabelGenerator labelMaker = new ParentPathLabelGenerator();
+            
+            
+            BalancedPathFilter pathFilter = new BalancedPathFilter(randNumGen, allowedExtensions, labelMaker);
+            InputSplit[] filesInDirSplit = filesInDir.sample(pathFilter, 80, 20);
+            InputSplit trainData = filesInDirSplit[0];
+            InputSplit testData = filesInDirSplit[1];
+            
+            
+            ImageRecordReader recordReader = new ImageRecordReader(28, 28, nChannels, labelMaker);
+            int outputNum = 5749;
+            
+            recordReader.initialize(trainData);
+            org.nd4j.linalg.dataset.api.iterator.DataSetIterator dataIter = new RecordReaderDataSetIterator(recordReader, 10, 784, outputNum);
+            
+            int contador = 0;
+//            while (dataIter.hasNext()) {
+//               DataSet ds =  dataIter.next();
+//               System.out.println(ds);
+//                contador++;                
+//            }
+            
+            System.out.println("Num Clases: "+dataIter.getLabels().size());
+            int seed = 0;
+            
+            
+            
+            
+            
+            
+            
+            final int numRows = 40;
+            final int numColumns = 40;
+//            int nChannels = 3;
+//            int outputNum = LFWLoader.SUB_NUM_LABELS;
+//            int numSamples = LFWLoader.SUB_NUM_IMAGES-4;
+            int batchSize =   30;   // numSamples/10;
+            int iterations = 5;
+            int splitTrainNum = (int) (batchSize*.8);
+//            int seed = 123;
+            int listenerFreq = iterations/5;
+            boolean useSubset = true;
+            DataSet lfwNext;
+            SplitTestAndTrain trainTest;
+            DataSet trainInput;
+            List<INDArray> testInput = new ArrayList<>();
+            List<INDArray> testLabels = new ArrayList<>();
+            
+            
+            
+            
+            
+            
+            
+            
+            MultiLayerConfiguration.Builder builder = new NeuralNetConfiguration.Builder()
                 .seed(seed)
                 .iterations(iterations)
-                .activation("relu")
+                .regularization(true).l2(0.0005)
+                .learningRate(0.01)//.biasLearningRate(0.02)
+                //.learningRateDecayPolicy(LearningRatePolicy.Inverse).lrPolicyDecayRate(0.001).lrPolicyPower(0.75)
                 .weightInit(WeightInit.XAVIER)
-                .gradientNormalization(GradientNormalization.RenormalizeL2PerLayer)
                 .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
-                .learningRate(0.01)
-                .momentum(0.9)
-                .regularization(true)
-                .updater(Updater.ADAGRAD)
-                .useDropConnect(true)
-                .list(6)
-                .layer(0, new ConvolutionLayer.Builder(4, 4)
-                        .name("cnn1")
+                .updater(Updater.NESTEROVS).momentum(0.9)
+                .list()
+                .layer(0, new ConvolutionLayer.Builder(5, 5)
+                        //nIn and nOut specify depth. nIn here is the nChannels and nOut is the number of filters to be applied
                         .nIn(nChannels)
                         .stride(1, 1)
                         .nOut(20)
+                        .activation("identity")
                         .build())
-                .layer(1, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX, new int[]{2, 2})
-                        .name("pool1")
-                        .build())
-                .layer(2, new ConvolutionLayer.Builder(3, 3)
-                        .name("cnn2")
-                        .stride(1,1)
-                        .nOut(40)
-                        .build())
-                .layer(3, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX, new int[]{2, 2})
-                        .name("pool2")
-                        .build())
-                .layer(2, new ConvolutionLayer.Builder(3, 3)
-                        .name("cnn3")
-                        .stride(1,1)
-                        .nOut(60)
-                        .build())
-                .layer(3, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX, new int[]{2, 2})
-                        .name("pool3")
-                        .build())
-                .layer(2, new ConvolutionLayer.Builder(2, 2)
-                        .name("cnn3")
-                        .stride(1,1)
-                        .nOut(80)
-                        .build())
-                .layer(4, new DenseLayer.Builder()
-                        .name("ffn1")
-                        .nOut(160)
-                        .dropOut(0.5)
-                        .build())
-                .layer(5, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
-                        .nOut(outputNum)
-                        .activation("softmax")
-                        .build())
-                .backprop(true).pretrain(false);
-        new ConvolutionLayerSetup(builder,numRows,numColumns,nChannels);
-
-        MultiLayerNetwork model = new MultiLayerNetwork(builder.build());
-        model.init();
-
-//        log.info("Train model....");
-        model.setListeners(Arrays.asList((IterationListener) new ScoreIterationListener(listenerFreq)));
-
-        while(lfw.hasNext()) {
-            lfwNext = lfw.next();
-            lfwNext.scale();
-            trainTest = lfwNext.splitTestAndTrain(splitTrainNum, new Random(seed)); // train set that is the result
-            trainInput = trainTest.getTrain(); // get feature matrix and labels for training
-            testInput.add(trainTest.getTest().getFeatureMatrix());
-            testLabels.add(trainTest.getTest().getLabels());
-            model.fit(trainInput);
+                    .layer(1, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX, new int[]{2, 2})
+                            .name("pool1")
+                            .build())
+                    .layer(2, new ConvolutionLayer.Builder(3, 3)
+                            .name("cnn2")
+                            .stride(1,1)
+                            .nOut(40)
+                            .build())
+                    .layer(3, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX, new int[]{2, 2})
+                            .name("pool2")
+                            .build())
+                    .layer(2, new ConvolutionLayer.Builder(3, 3)
+                            .name("cnn3")
+                            .stride(1,1)
+                            .nOut(60)
+                            .build())
+                    .layer(3, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX, new int[]{2, 2})
+                            .name("pool3")
+                            .build())
+                    .layer(2, new ConvolutionLayer.Builder(2, 2)
+                            .name("cnn3")
+                            .stride(1,1)
+                            .nOut(80)
+                            .build())
+                    .layer(4, new DenseLayer.Builder()
+                            .name("ffn1")
+                            .nOut(160)
+                            .dropOut(0.5)
+                            .build())
+                    .layer(5, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                            .nOut(outputNum)
+                            .activation("softmax")
+                            .build())
+                    .backprop(true).pretrain(false);
+            new ConvolutionLayerSetup(builder,numRows,numColumns,nChannels);
+            
+            MultiLayerNetwork model = new MultiLayerNetwork(builder.build());
+            model.init();
+            
+//            log.info("Train model....");
+            model.setListeners(Arrays.asList((IterationListener) new ScoreIterationListener(listenerFreq)));
+            
+            org.nd4j.linalg.dataset.api.iterator.DataSetIterator lfw = dataIter;
+            
+            while(lfw.hasNext()) {
+                lfwNext = lfw.next();
+                lfwNext.scale();
+                trainTest = lfwNext.splitTestAndTrain(splitTrainNum, new Random(seed)); // train set that is the result
+                trainInput = trainTest.getTrain(); // get feature matrix and labels for training
+                testInput.add(trainTest.getTest().getFeatureMatrix());
+                testLabels.add(trainTest.getTest().getLabels());
+                model.fit(trainInput);
+            }
+            
+//            log.info("Evaluate model....");
+            Evaluation eval = new Evaluation(lfw.getLabels());
+            for(int i = 0; i < testInput.size(); i++) {
+                INDArray output = model.output(testInput.get(i));
+                eval.eval(testLabels.get(i), output);
+            }
+            INDArray output = model.output(testInput.get(0));
+            eval.eval(testLabels.get(0), output);
+//            log.info(eval.stats());
+//            log.info("****************Example finished********************");
+        } catch (IOException ex) {
+            java.util.logging.Logger.getLogger(CNNLFWExample.class.getName()).log(Level.SEVERE, null, ex);
         }
-
-//        log.info("Evaluate model....");
-        Evaluation eval = new Evaluation(lfw.getLabels());
-        for(int i = 0; i < testInput.size(); i++) {
-            INDArray output = model.output(testInput.get(i));
-            eval.eval(testLabels.get(i), output);
-        }
-        INDArray output = model.output(testInput.get(0));
-        eval.eval(testLabels.get(0), output);
-//        log.info(eval.stats());
-//        log.info("****************Example finished********************");
 
     }
 
